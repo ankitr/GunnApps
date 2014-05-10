8c557ab6dc1b84e81ead9a1acccdbff03cef5bac
Ankit Ranjan <me@ankit.io>
2014-05-09 23:08:17 -0700

    Created a simple user system that works.

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,13 +1,49 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import hashlib
+
+from bson.objectid import ObjectId
+
 from flask import Blueprint
+from flask import current_app
 from flask import jsonify
+from flask import redirect
 from flask import render_template
 from flask import request
 
+from flask.ext.login import current_user
+from flask.ext.login import LoginManager
+from flask.ext.login import login_required
+from flask.ext.login import login_user
+from flask.ext.login import logout_user
+
+import db
+from ..logs import log
+
+from mongokit import MultipleResultsFound
+
+
 blueprint = Blueprint('dashboard', __name__)
 
+login_manager = LoginManager()
+
+
+# Sets up Flask Login.
+@blueprint.record_once
+def on_load(state):
+    login_manager.init_app(state.app)
+
+@login_manager.user_loader
+def load_user(userid):
+    return db.users.User.one({'_id': ObjectId(userid)})
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    return render_template('main.html', need_auth=True)
+
+
+# Creates our views.
 @blueprint.route('/', methods=['GET'])
 def land():
     """Returns the landing page."""
@@ -17,9 +53,54 @@ def land():
 def login():
     email = request.form['email']
     password = request.form['password']
+    try:
+        user = db.users.User.one({'email':email})
+    except MultipleResultsFound as e:
+        log.critical('Multiple users with the same email address found!')
+        log.exception(e)
+    if not user or (hashlib.sha512(password).hexdigest() != user['password']):
+        return render_template('main.html', invalid=True, email=email)
+    login_user(user)
+    return redirect('/home', code=303)
 
 @blueprint.route('/register', methods=['GET'])
-@blueprint.route('/', methods=['GET'])
 def contemplate():
     """Returns the registration page."""
     return render_template('register.html')
+
+@blueprint.route('/register', methods=['POST'])
+def register():
+    """Creates a user account."""
+    code = request.form['code']
+    email = request.form['email']
+    name = request.form['name'].title()
+    password = request.form['password']
+    if not db.codes.one({'code':code}):
+        return render_template('register.html', invalid=True)
+    if db.codes.one({'email':email}):
+        # The user exists.
+        return render_template('register.html', invalid=True)
+    # Remove the now-used code.
+    db.codes.remove({'code':code})
+    # Code is valid and user is free.
+    user = db.users.User()
+    user['email'] = email
+    user['name'] = name
+    user['password'] = hashlib.sha512(password).hexdigest()
+    user.save()
+    login_user(user)
+    return redirect('/home', code=303)
+
+
+
+@blueprint.route('/home')
+@login_required
+def home():
+    return render_template('loggedin.html', user=current_user)
+
+@blueprint.route('/logout')
+@login_required
+def logout():
+    """Logs out the user."""
+    logout_user()
+    return redirect('/')
