7495e815fa0655ccf78e252c9389f008a32ab0ca
Ankit Ranjan <me@ankit.io>
2014-07-19 23:38:17 -0700

    Bug fixes (I'll test next time)

--- gunnapps/apps/db.py
+++ gunnapps/apps/db.py
@@ -7,7 +7,7 @@ import string
 from mongokit import Connection
 from mongokit import Document
 
-from pymongo.objectid import ObjectId
+from bson.objectid import ObjectId
 
 # Create the MongoDB 
 connection = Connection()
@@ -25,7 +25,7 @@ class User(Document):
     __database__ = 'auth'
 
     structure = {
-		'student_id': int
+        'student_id': int,
         'email': basestring,
         'password': basestring,
         'name': basestring,
@@ -54,36 +54,36 @@ class User(Document):
     def get_id(self):
         return unicode(self['student_id'])
 
-	def get_student_email(self):
-		# TODO: Check how PAUSD handles middle initials, etc.
-		first_name = self['name'].split(' ')[0]
-		last_name = self['name'].split(' ')[-1]
-		last_initial = last_name[0]
-		student_id_tail = str(self['student_id'])[-5:]
-		email = '%s.%s.%s@palo-alto.edu' % (first_name, last_initial, student_id_tail)
-		return email
+    def get_student_email(self):
+        # TODO: Check how PAUSD handles middle initials, etc.
+        first_name = self['name'].split(' ')[0]
+        last_name = self['name'].split(' ')[-1]
+        last_initial = last_name[0]
+        student_id_tail = str(self['student_id'])[-5:]
+        email = '%s.%s.%s@palo-alto.edu' % (first_name, last_initial, student_id_tail)
+        return email
 
 
 @connection.register
 class App(Document):
-	"""Third-party applications on our framework."""
+    """Third-party applications on our framework."""
 
-	__collection__ = 'apps'
-	__database__ = 'auth'
+    __collection__ = 'apps'
+    __database__ = 'auth'
 
-	structure = {
-		'owner': ObjectId,
-		'name': basestring,
-		'id': basestring,
-		'secret': basestring
-	}
-	
-	required_fields = ['owner', 'name', 'id', 'secret']
+    structure = {
+        'owner': ObjectId,
+        'name': basestring,
+        'id': basestring,
+        'secret': basestring
+    }
+    
+    required_fields = ['owner', 'name', 'id', 'secret']
 
-	def _generate_secret(self):
-		return ''.join(random.choice(string.ascii_uppercase + string.digits) for i in xrange(32))
-	
-	def refresh_secret(self):
-		self['secret'] = self._generate_secret()
-		self.save(validate=True)
-		return self['secret']
+    def _generate_secret(self):
+        return ''.join(random.choice(string.ascii_uppercase + string.digits) for i in xrange(32))
+    
+    def refresh_secret(self):
+        self['secret'] = self._generate_secret()
+        self.save(validate=True)
+        return self['secret']
